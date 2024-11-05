b7fbb9783447fccc0a48fca69939660fa7f59ca9
alexandershutai <a.shutai@agima.ru>
2024-11-05 09:39:45 +0300

    add filament

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,8 @@
 <?php
-use Illuminate\Support\Facades\Route;
+
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Redirect;
-use App\Http\Controllers\TagController;
-use App\Http\Controllers\ArticleController;
+use Illuminate\Support\Facades\Route;
 
-
-Route::resource('tags', TagController::class)->missing(function (Request $request) {
-    return Redirect::route('tags.index');
-})->except('edit', 'create');
-
-Route::resource('articles', ArticleController::class)->missing(function (Request $request) {
-    return Redirect::route('articles.index');
-})->except('edit', 'create');
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+    return $request->user();
+});
